0d1a793c264d03837d6b79ecbfe99dc2fc901d15
Akhil Lawrence <akhilputhiry@gmail.com>
2017-04-02 16:02:18 +0530

    updated travis

--- ssms/ssms/settings/travis.py
+++ ssms/ssms/settings/travis.py
@@ -1,12 +1,1 @@
 from local import *
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'tenant_schemas.postgresql_backend',
-        'NAME': 'TESTS',
-        'USER': 'postgresql',
-        'PASSWORD': '',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
-}
